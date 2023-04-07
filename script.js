c9233d3c1c452d032ef2929a2aa3aa3504639943
ndrbr <andrey.burdak@gmail.com>
2023-04-08 01:16:44 +0700

    Changed page navigation

---
+++ script.js
@@ -0,0 +1,10 @@
+// Page navigation
+
+document.querySelector(".nav").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
